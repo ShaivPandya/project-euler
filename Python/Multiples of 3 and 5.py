a7c5e49f05b9c76d71592cd5f85436718ec476d6
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2020-10-03 11:03:38 -0600

    Add files via upload

---
+++ Python/Multiples of 3 and 5.py
@@ -0,0 +1,11 @@
+sum = 0
+
+for i in range(1000):
+    if i%3 == 0 or i%5 == 0:
+        sum += i
+    
+print(sum)
+
+# print(sum(i for i in range(1000) if not (i%3 and i%5)))
+
+# print(sum(i for i in range(1000) if i%3 == 0 or i%5 == 0))
